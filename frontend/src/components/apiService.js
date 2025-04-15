f172627d17a42fbc17ecd49acf4c442b5d441d0f
kuldeep-singh111 <ks6201709@gmail.com>
2025-04-15 17:25:16 +0530

    live port added

--- frontend/src/components/apiService.js
+++ frontend/src/components/apiService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // baseURL: https://api.lyaim.com
 
 const API = axios.create({
-  baseURL: "lyaimtechdevelopers-production.up.railway.app" || "http://localhost:9000",
+  baseURL: "https://lyaimtechdevelopers-production.up.railway.app" || "http://localhost:9000",
   // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:9000",
   withCredentials: true,
 });
